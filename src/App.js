ca0c624150c8ce13986886df00eac937b3b745f1
Alex-Colgan <alexandercolgan@gmail.com>
2022-11-02 18:06:56 +0000

    map faq update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Postcard from "./Components/Postcard.js";
 import Box from "./Components/box.js"; // Imports the Box.js
 import Form from "./Components/Form/index.js"; // Imports the index.js from the Form folder
 import Accordion from "./Components/Accordion.js";
+import Accordion2 from "./Components/Accordion2.js";
+import { data } from "./Components/data";
 
 function App() {
   return (
@@ -27,9 +29,24 @@ function App() {
         */}
 
         <div>
+          <h2>FAQ</h2>
           <Accordion title="How do I contact you?" />
           <Accordion title="What is the best way to use your product?" />
           <Accordion title="Is there a phone number?" />
+        </div>
+
+        <div>
+          <h2>FAQ2</h2>
+          {
+            // Use the map function to map the data to output multiple accordions.
+            data.map(function(item, index){
+              return (
+                <Accordion2 title={item.question}>
+                  {item.answer}
+                </Accordion2>
+              )
+            })
+          }
         </div>
 
         <div>
